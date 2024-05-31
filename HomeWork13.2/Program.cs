4bea6d612daa5b039db212edd64ec042e46851d8
Albina-K <kamalieva.albish@mail.ru>
2024-05-31 20:32:01 +0300

    Добавьте файлы проекта.

---
+++ HomeWork13.2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace HomeWork2;
+
+class Program
+{
+    public static LinkedList<string> linkedList = new LinkedList<string>();
+    static void Main(string[] args)
+    {
+        string text = File.ReadAllText(@"C:\Users\Альбина\Desktop\HW13.txt");
+
+        var watchOne = Stopwatch.StartNew();
+
+        linkedList.AddFirst(text);
+
+        Console.WriteLine($"Вставка в LinkedList:{watchOne.Elapsed.TotalMilliseconds} мс");
+
+    }
+
+}
---
+++ HomeWork13/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace HomeWork1;
+
+class Program
+{
+    public static List<string> list = new List<string>();
+    static void Main(string[] args)
+    {
+        string text = File.ReadAllText(@"C:\Users\Альбина\Desktop\HW13.txt");
+
+        var watchOne = Stopwatch.StartNew();
+
+        list.Add(text);
+
+        Console.WriteLine($"Вставка в List:{watchOne.Elapsed.TotalMilliseconds} мс");
+    }
+        
+}
