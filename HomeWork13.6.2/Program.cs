fe24e302662b2ba188e48c1f695476e94071ab44
Albina-K <kamalieva.albish@mail.ru>
2024-06-01 01:38:19 +0300

    процесс. задание 2

---
+++ HomeWork13.6.2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace HomeWork2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string text = File.ReadAllText(@"C:\Users\Альбина\Desktop\HW13.2.txt");
+
+        string textUp = text.ToUpper();
+
+        var noPunctuationText = new string(textUp.Where(c => !char.IsPunctuation(c)).ToArray());
+
+        char[] delimiters = new char [] { ' ', '\r', '\n' };
+
+        var words = noPunctuationText.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+        var hSet = new HashSet<string>(words);
+
+        var repeatedWords = new Dictionary<string, int>();
+
+        foreach (var word in hSet) 
+        {
+            repeatedWords.Add(word, 0);        
+        }
+
+        foreach (var word in words) 
+        {
+        if (repeatedWords.ContainsKey(word)) 
+            { 
+            
+            }
+        }
+    }
+
+}
